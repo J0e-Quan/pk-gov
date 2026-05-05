dd73e3ffad3b2032bb6ec6529d6a52af9767c367
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 21:34:29 +0800

    feat(ui): add moi page

---
+++ src/ministries/immigration.js
@@ -0,0 +1,14 @@
+import '../assets/styles/global.css'
+import '../assets/styles/immigration.css'
+import Lottie from 'lottie-web'
+import logo from '../assets/lottie/immigration.json'
+
+const logoContainer = document.querySelector('.ministry-of-immigration.logo-lottie')
+
+Lottie.loadAnimation({
+  container: logoContainer,    
+  renderer: 'svg',         
+  loop: false,             
+  autoplay: true,        
+  animationData: logo
+});
