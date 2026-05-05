a1c9dc9b2381b46a4c89cebd8b18c841ed773f22
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 17:13:12 +0800

    feat(ui): add return btn at end of cm page

--- src/ministries/central.js
+++ src/ministries/central.js
@@ -2,7 +2,6 @@ import '../assets/styles/global.css'
 import '../assets/styles/central.css'
 import Lottie from 'lottie-web'
 import central from '../assets/lottie/central.json'
-import pbb from'../assets/pictures/ministers/pbb.webp'
 
 const centralContainer = document.querySelector('.central-ministry.logo-lottie')
 
