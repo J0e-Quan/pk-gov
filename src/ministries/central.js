3af2e6f3837b2bc88215bfa7945b9d88ce03da8b
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 14:51:06 +0800

    fix(css): rename cm.css to central.css
    make css filenames consistent with html and js filenames

--- src/ministries/central.js
+++ src/ministries/central.js
@@ -1,5 +1,5 @@
 import '../assets/styles/global.css'
-import '../assets/styles/cm.css'
+import '../assets/styles/central.css'
 import Lottie from 'lottie-web'
 import cm from '../assets/lottie/cm.json'
 
