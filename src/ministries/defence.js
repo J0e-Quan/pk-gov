2150e03ebd506a4ff126c4252ff2d934b957f7b0
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 21:59:10 +0800

    feat(ui): add mod page
    todo list:
    - use simplified css template for all ministries
    - check for any spelling/grammar errors in existing pages
    - begin setting up (PLAN FIRST BEFORE DOING) info pages

---
+++ src/ministries/defence.js
@@ -0,0 +1,14 @@
+import '../assets/styles/global.css'
+import '../assets/styles/defence.css'
+import Lottie from 'lottie-web'
+import logo from '../assets/lottie/defence.json'
+
+const logoContainer = document.querySelector('.ministry-of-defence.logo-lottie')
+
+Lottie.loadAnimation({
+  container: logoContainer,    
+  renderer: 'svg',         
+  loop: false,             
+  autoplay: true,        
+  animationData: logo
+});
