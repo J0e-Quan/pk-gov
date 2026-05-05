1057993bc02bd292ea9c70353c12aad66fb3342f
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 21:47:17 +0800

    feat(ui): add mot page

---
+++ src/ministries/technology.js
@@ -0,0 +1,14 @@
+import '../assets/styles/global.css'
+import '../assets/styles/technology.css'
+import Lottie from 'lottie-web'
+import logo from '../assets/lottie/technology.json'
+
+const logoContainer = document.querySelector('.ministry-of-technology.logo-lottie')
+
+Lottie.loadAnimation({
+  container: logoContainer,    
+  renderer: 'svg',         
+  loop: false,             
+  autoplay: true,        
+  animationData: logo
+});
