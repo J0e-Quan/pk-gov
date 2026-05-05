ce81cd1c232b170bfbc4536e7735debb689306a0
Lim Joe Quan <joequanlim@gmail.com>
2026-05-05 21:08:35 +0800

    feat(ui): add moh page

---
+++ src/ministries/health.js
@@ -0,0 +1,14 @@
+import '../assets/styles/global.css'
+import '../assets/styles/health.css'
+import Lottie from 'lottie-web'
+import logo from '../assets/lottie/health.json'
+
+const logoContainer = document.querySelector('.ministry-of-health.logo-lottie')
+
+Lottie.loadAnimation({
+  container: logoContainer,    
+  renderer: 'svg',         
+  loop: false,             
+  autoplay: true,        
+  animationData: logo
+});
