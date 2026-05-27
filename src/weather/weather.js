f33f4ba867ca917b12b72a19acfbf880ecabad96
Lim Joe Quan <joequanlim@gmail.com>
2026-05-27 20:39:05 +0800

    feat(code): show current weather temp and icon

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -1,25 +1,84 @@
 import '../assets/styles/global.css'
 import '../assets/styles/weather.css'
+import sunny from '../assets/weather-icons/sunny.svg'
+import night from '../assets/weather-icons/night.svg'
+import cloudy from '../assets/weather-icons/cloudy.svg'
+import sunnyCloudy from '../assets/weather-icons/sunny-cloudy.svg'
+import nightCloudy from '../assets/weather-icons/night-cloudy.svg'
+import rain from '../assets/weather-icons/rain.svg'
+import thunderstorm from '../assets/weather-icons/thunderstorm.svg'
+import warning from '../assets/weather-icons/warning.svg'
+import generic from '../assets/weather-icons/generic.svg'
 
-const currentWeather = document.querySelector('.current-weather.content')
 const forecastToday = document.querySelector('.forecast-today.content')
 const forecastWeek = document.querySelector('.forecast-week.content')
 
 async function getData() {
-  const data = await fetch('https://api.open-meteo.com/v1/forecast?latitude=5.4112&longitude=100.3354&daily=weather_code,apparent_temperature_max&hourly=precipitation_probability,weather_code,is_day,apparent_temperature&current=is_day,precipitation,weather_code,apparent_temperature&timezone=Asia%2FSingapore').then(function(response) {
-      return response.json()
-    }).then(function(response) {
-      return response
-    }).catch(function(error) {
-      console.log(error)
-      alert('There was an error fetching weather data. Please ensure you are online, and refresh the page to try again.')
-    })
-  return data
+  try {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=5.4112&longitude=100.3354&daily=weather_code,apparent_temperature_max,precipitation_probability_max&hourly=apparent_temperature,precipitation_probability,weather_code,is_day&current=apparent_temperature,weather_code,precipitation,is_day&timezone=Asia%2FSingapore')
+    const data = response.json()
+    // data cannot be checked with .ok because it has been parsed into json
+    if (!response.ok) {
+      console.error(data.reason)
+      return null
+    }
+    return data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
-function displayData(data) {
+function checkData(data) {
   console.log(data)
+  if (data === null) {
+    return alert("There was an error in fetching weather data! Please make sure you're online and refresh the page to try again.")
+  } 
+  showCurrentWeather()
+}
+
+function displayIcon(weatherCode, isDay){
+  // clear sky
+  if ((weatherCode === 0 || weatherCode === 1) && isDay) {
+    return sunny
+  } else if ((weatherCode === 0 || weatherCode === 1) && !isDay) {
+    return night
+  } else
+  // partly cloudy
+  if (weatherCode === 2 && isDay) {
+    return sunnyCloudy
+  } else if (weatherCode === 2 && !isDay) {
+    return nightCloudy
+  } else
+  // cloudy 
+  if (weatherCode === 3 || weatherCode === 45 || weatherCode === 48) {
+    return cloudy
+  } else 
+  // rain
+  if (weatherCode === 51 || weatherCode === 53 || weatherCode === 55 || weatherCode === 61 || weatherCode === 63 || weatherCode === 65 || weatherCode === 80 || weatherCode === 81) {
+    return rain
+  } else
+  // thunderstorm
+  if (weatherCode === 82 || weatherCode === 95 || weatherCode === 96 || weatherCode === 99) {
+    return thunderstorm
+  } else {
+    return generic
+  }
+}
+
+function showCurrentWeather() {
+  const currentWeatherLoader = document.querySelector('.current-weather.loader')
+  currentWeatherLoader.remove()
+  const currentWeather = document.querySelector('.current-weather.content')
+  const currentWeatherIcon = document.createElement('img')
+  currentWeatherIcon.classList.add('current-weather', 'icon')
+  currentWeatherIcon.src = displayIcon(data.current.weather_code, data.current.is_day)
+  currentWeather.appendChild(currentWeatherIcon)
+  const currentWeatherTemp = document.createElement('h3')
+  currentWeatherTemp.classList.add('current-weather', 'temp')
+  currentWeatherTemp.textContent = data.current.apparent_temperature + '°C'
+  currentWeather.appendChild(currentWeatherTemp)
 }
 
 const data = await getData()
-displayData(data)
+checkData(data)
