61300790b0bea8f0f9681bbbe58bc32ff2b661fd
Lim Joe Quan <joequanlim@gmail.com>
2026-05-27 19:01:10 +0800

    feat(ui): prepare loading animation
    also updated hero text to remove mention of data.gov.my (open-meteo
    will be used for all weather info as it has more generous rate limits)

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -1,2 +1,4 @@
 import '../assets/styles/global.css'
 import '../assets/styles/weather.css'
+
+// api call link: https://api.open-meteo.com/v1/forecast?latitude=5.4112&longitude=100.3354&daily=weather_code,apparent_temperature_max&hourly=precipitation_probability,weather_code,is_day,apparent_temperature&current=is_day,precipitation,weather_code,apparent_temperature&timezone=Asia%2FSingapore
