0ab286ebb75180d49f6f2d3d6a9448559d0aca2c
Lim Joe Quan <joequanlim@gmail.com>
2026-06-21 21:48:46 +0800

    feat(js): write function for creating sb client
    located in pibss-common because it will be used by the other two pibss
    js modules

--- src/pibss/pibss-common.js
+++ src/pibss/pibss-common.js
@@ -1,5 +1,6 @@
 import '../assets/styles/global.css'
 import '../assets/styles/content-page.css'
+import { createClient } from '@supabase/supabase-js'
 
 // code for opening pagefind modal for search-mobile
 document.addEventListener('DOMContentLoaded', () => {
@@ -18,3 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient('https://nsqlmhmrtprfrykovvqg.supabase.co', 'sb_publishable_4hpz79Ywi9UaYSfk_KxQ1g_Z8XDr4Lg')
