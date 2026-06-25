b670e71466b5be93cf750aa2bd2662be23e96653
Lim Joe Quan <joequanlim@gmail.com>
2026-06-25 21:09:59 +0800

    feat(pibss): implement auth

--- src/pibss/pibss-login.js
+++ src/pibss/pibss-login.js
@@ -1,8 +1,29 @@
 import { supabase } from "./pibss-common.js";
 
-async function signIn() {
+const loginButton = document.querySelector('.login-form button')
+loginButton.addEventListener('click', getLoginDetails)
+
+function getLoginDetails() {
+  const emailInput = document.getElementById('email')
+  const pwInput = document.getElementById('pw')
+  const emailAddress = emailInput.value
+  const pw = pwInput.value
+  if (emailAddress !== '' && pw !== '') {
+    signIn(emailAddress, pw)
+  } else {
+    alert('Please input login details!')
+  }
+}
+
+async function signIn(emailAddress, pw) {
   const { data, error } = await supabase.auth.signInWithPassword({
-    email: 'valid.email@supabase.io',
-    password: 'example-password',
+    email: emailAddress,
+    password: pw,
   })
+  if (error) {
+    console.error(error)
+    alert('Incorrect login details!')
+  } else {
+    window.location.href = '/pibss/manage/'
+  }
 }
