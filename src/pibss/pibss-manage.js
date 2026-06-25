de054b981b9a263f936df5e5ff03ae375426d391
Lim Joe Quan <joequanlim@gmail.com>
2026-06-25 21:49:41 +0800

    feat(pibss): setup UI when form begins

--- src/pibss/pibss-manage.js
+++ src/pibss/pibss-manage.js
@@ -13,3 +13,58 @@ async function checkUserAuthentication() {
     // User is authorized
   }
 }
+
+const registerButton = document.querySelector('.register-button')
+const updateLocationButton = document.querySelector('.update-location-button')
+const content = document.querySelector('.content')
+// initially, form is done because there is no form to begin with
+let isFormDone = true
+
+registerButton.addEventListener('click', beginRegister)
+updateLocationButton.addEventListener('click', beginUpdateLocation)
+
+function changeReturnButton() {
+  const returnButton = document.querySelector('.return')
+  returnButton.textContent = 'Return to dashboard'
+  returnButton.href = '/pibss/manage/'
+  window.addEventListener('beforeunload', (event) => {
+    if (isFormDone === false) {
+      event.preventDefault()
+      event.returnValue = ''
+    }
+  })
+}
+
+function showProgressUI(formTitle, formSteps) {
+  const progress = document.createElement('section')
+  progress.classList.add('progress')
+  const title = document.createElement('h2')
+  title.classList.add('progress-title')
+  title.textContent = formTitle + ': Step ' 
+  const stepNumber = document.createElement('span')
+  stepNumber.classList.add('progress-number')
+  title.appendChild(stepNumber)
+  title.append('1 of ', formSteps)
+  progress.appendChild(title)
+  const progressBar = document.createElement('div')
+  progressBar.classList.add('progress-bar')
+  progress.appendChild(progressBar)
+  content.appendChild(progress)
+  changeReturnButton()
+}
+
+function beginRegister() {
+  content.innerHTML = ''
+  // show start registering splash screen n stuff
+}
+
+function beginUpdateLocation() {
+  content.innerHTML = ''
+  showProgressUI('Update plushie location', '3')
+  isFormDone = false
+  renderPlushieSelectionForm()
+}
+
+function renderPlushieSelectionForm() {
+
+}
