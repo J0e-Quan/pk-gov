62f0011e6935d554dcb64bc54021281da0ce1434
Lim Joe Quan <joequanlim@gmail.com>
2026-06-25 21:13:52 +0800

    feat(pibss): check user auth in manage.html
    user auth status is immediately checked when entering manage.html, if
    the user isnt logged in they are redirected to the login page

--- src/pibss/pibss-manage.js
+++ src/pibss/pibss-manage.js
@@ -1,1 +1,15 @@
 import { supabase } from "./pibss-common.js";
+
+// check if user is authenticated immediately
+checkUserAuthentication()
+
+async function checkUserAuthentication() {
+  const { data: { session }, error } = await supabase.auth.getSession();
+
+  if (error || !session) {
+    // No active session found, user is redirected the login page immediately
+    window.location.href = '/pibss/login/'
+  } else {
+    // User is authorized
+  }
+}
