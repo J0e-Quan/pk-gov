3e05418015c24da44754b8f98e7d36b57c87c2fb
Lim Joe Quan <joequanlim@gmail.com>
2026-08-09 23:59:14 +0800

    refactor(code): change holiday fetching method
    use holidays/between to get holidays between today and end of year, so
    that previous holidays do not need to be trimmed out/filtered separately

--- src/timeline/timeline.js
+++ src/timeline/timeline.js
@@ -1,4 +1,29 @@
-function getCurrentYear() {
+function getCurrentDate() {
   const currentDate = new Date
-  return currentDate.getFullYear()
+  // en-CA region uses YYYY-MM-DD by default, which is the same format used by the calendar API
+  return currentDate.toLocaleDateString('en-CA')
 }
+
+function getEndDate() {
+  const currentDate = new Date
+  const currentYear = currentDate.getFullYear()
+  // last day of every year is always Dec 31st
+  return currentYear + "-12-31"
+}
+
+async function getData(currentDate, endDate) {
+  try {
+    const response = await fetch("https://mycal-api.huijun00100101.workers.dev/v1/holidays/between?start=" + currentDate + "&end=" + endDate + "&state=penang")
+    const data = response.json()
+    if (!response.ok) {
+      console.error(data.reason)
+      return null
+    }
+    return data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+const data = await getData(getCurrentDate(), getEndDate())
